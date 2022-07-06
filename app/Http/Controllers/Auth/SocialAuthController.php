4b8e1193476cc1d8eb8da37f8a7d481c2103945f
chaiqou <lomtadzenikusha@gmail.com>
2022-07-07 00:23:58 +0400

    feat: register user with socialite and return token

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
+use App\Models\SocialAccount;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialAuthController extends Controller
@@ -15,6 +17,39 @@ class SocialAuthController extends Controller
 	public function handleProviderCallback()
 	{
 		$user = Socialite::driver('google')->stateless()->user();
-		dd($user);
+
+		if (!$user)
+		{
+			// aq unda davabruno json romelic gamoitans daifelebis shemtxvevashi erors
+			dd('failed');
+		}
+
+		$databaseUser = User::whereEmail($user->email)->first();
+
+		if (!$databaseUser)
+		{
+			$databaseUser = User::create([
+				'name'     => $user->name,
+				'email'    => $user->email,
+				'password' => bcrypt($user->id),
+			]);
+
+			$socialAccount = SocialAccount::create([
+				'user_id'          => $databaseUser->id,
+				'provider_user_id' => $user->id,
+				'provider'         => 'google',
+			]);
+		}
+		else
+		{
+			dd('this user exists');
+		}
+
+		// login user and get token
+		$token = $databaseUser->createToken('authToken')->plainTextToken;
+
+		return response()->json([
+			'access_token' => $token,
+		]);
 	}
 }
