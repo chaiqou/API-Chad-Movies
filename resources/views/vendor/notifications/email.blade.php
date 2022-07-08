4f6a2a49b00d9feb29948eab053c3457f6b37ae0
chaiqou <lomtadzenikusha@gmail.com>
2022-07-08 19:11:27 +0400

    style: make email responsive

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -11,9 +11,13 @@
 <body>
 
     <div
-        style="height: 600px;  text-align:center; margin-top: 6rem; background-image: linear-gradient(187.16deg, #181623 0.07%, #191725 51.65%, #0D0B14 98.75%);   background-repeat: no-repeat;  background-attachment: fixed; margin: 0; padding: 0; box-sizing: border-box">
+        style="height: 600px;  text-align: left; margin-top: 8rem; background-image: linear-gradient(187.16deg, #181623 0.07%, #191725 51.65%, #0D0B14 98.75%);   background-repeat: no-repeat;  background-attachment: fixed;">
         <div style="padding: 0 auto; ">
-            <img src="https://i.ibb.co/MR96Jzx/Vector.png" alt="Vector" border="0" style="margin-top: 5rem">
+            <p style="margin-top: ;">
+                <img src="https://i.ibb.co/MR96Jzx/Vector.png" alt="Vector" border="0"
+                    style="text-align: center; display: block; margin-left: auto; margin-right: auto;">
+            </p>
+
             <p style="color: #DDCCAA; text-align: center;">MOVIE QUOTES</p>
             <p
                 style="text-align: left; margin-left: 3rem; line-height: 22px;  font-size: 18px; font-weight: 400; color: white;">
@@ -24,24 +28,26 @@
                 Thanks for joining Movie quotes! We really appreciate it. Please click the button below to verify your
                 account:
             </p>
-            <div style="">
-                @isset($actionText)
-                    <?php
-                    $color = match ($level) { 'success', 'error' => $level,  default => 'primary' };
-                    ?>
-                    @component('mail::button', ['url' => $actionUrl, 'color' => 'red'])
-                        Verify account
-                    @endcomponent
-                @endisset
 
-            </div>
+            <p>
+                <a href="{{ $actionUrl }}"" style="display: inline-block; margin-left: 3rem; 
+                padding:0.35em 1.2em; border:0.1em solid red; border-radius:0.12em; box-sizing: 
+                border-box; text-decoration: none; font-size: 1rem; font-weight: 400; color: white; background-color: #E31221; border-radius: 4px">
+                Verify account</a>
+            </p>
+
+
+
             <p
                 style="text-align: left; margin-left: 3rem; line-height: 22px;  font-size: 18px; font-weight: 400; color: white;">
                 If clicking doesn't work, you can try copying and pasting it to your browser: </p>
-            <a style='color: #DDCCAA; text-align: left;  white-space: normal;'
-                href="{{ $actionUrl }}">{{ $actionUrl }}</a>
+            <p>
+                <a style='display: inline-block; margin-left: 3rem; color: #DDCCAA;  white-space: normal;'
+                    href="{{ $actionUrl }}">{{ $actionUrl }}</a>
+            </p>
+
             <p
-                style="text-align: left; margin-left: 3rem; line-height: 22px;  font-size: 18px; font-weight: 400; color: white;">
+                style="text-align: left; margin-left: 3rem; line-height: 22px; margin-top: 1rem;  font-size: 18px; font-weight: 400; color: white;">
                 If you have any problems, please contact us: support@moviequotes.ge </p>
             <p
                 style="text-align: left; margin-left: 3rem; line-height: 22px;  font-size: 18px; font-weight: 400; color: white;">
@@ -50,6 +56,6 @@
     </div>
 
 </body>
-z
+
 
 </html>
