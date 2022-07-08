6510b7e5b124cb2ec0b6cf611c31fe58a66e4c81
chaiqou <lomtadzenikusha@gmail.com>
2022-07-08 18:29:35 +0400

    fix: email templage for gmail , change mailtrap to gmail server

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -8,23 +8,23 @@
 
 </head>
 
-<body
-    style="height: 100%; width: 100vw; background-image: linear-gradient(187.16deg, #181623 0.07%, #191725 51.65%, #0D0B14 98.75%); background-repeat: no-repeat;  background-attachment: fixed; margin: 0; padding: 0; box-sizing: border-box; margin-left: 5vw">
+<body>
 
     <div
-        style="height: 100%;  text-align:center;  display: flex; flex-direction: column; align-content: flex-start; margin-top: 20px">
+        style="height: 600px;  text-align:center; margin-top: 6rem; background-image: linear-gradient(187.16deg, #181623 0.07%, #191725 51.65%, #0D0B14 98.75%);   background-repeat: no-repeat;  background-attachment: fixed; margin: 0; padding: 0; box-sizing: border-box">
         <div style="padding: 0 auto; ">
-            <img src="https://i.ibb.co/MR96Jzx/Vector.png" alt="Vector" border="0" style="margin-top: 8px">
+            <img src="https://i.ibb.co/MR96Jzx/Vector.png" alt="Vector" border="0" style="margin-top: 5rem">
             <p style="color: #DDCCAA; text-align: center;">MOVIE QUOTES</p>
             <p
-                style="margin-top: 30px;  text-align: left; line-height: 150%;  font-size: 16px; font-weight: 400; color:white;">
+                style="text-align: left; margin-left: 3rem; line-height: 22px;  font-size: 18px; font-weight: 400; color: white;">
                 Hi Nikoloz
             </p>
-            <p style=" text-align: left; line-height: 150%;  font-size: 16px; font-weight: 400; color:white;">
+            <p
+                style="text-align: left; margin-left: 3rem; line-height: 22px;  font-size: 18px; font-weight: 400; color: white;">
                 Thanks for joining Movie quotes! We really appreciate it. Please click the button below to verify your
                 account:
             </p>
-            <div style="position: relative; right: 45vw">
+            <div style="">
                 @isset($actionText)
                     <?php
                     $color = match ($level) { 'success', 'error' => $level,  default => 'primary' };
@@ -35,20 +35,21 @@
                 @endisset
 
             </div>
-            <p style=" text-align: left;  line-height: 150%;  font-size: 16px; font-weight: 400; color:white;">
+            <p
+                style="text-align: left; margin-left: 3rem; line-height: 22px;  font-size: 18px; font-weight: 400; color: white;">
                 If clicking doesn't work, you can try copying and pasting it to your browser: </p>
-            <a style='position: relative; right: 20.5vw; color: #DDCCAA; text-align: left;  white-space: normal;'
+            <a style='color: #DDCCAA; text-align: left;  white-space: normal;'
                 href="{{ $actionUrl }}">{{ $actionUrl }}</a>
             <p
-                style=" text-align: left; margin-top: 20px; line-height: 150%;  font-size: 16px; font-weight: 400; color:white;">
+                style="text-align: left; margin-left: 3rem; line-height: 22px;  font-size: 18px; font-weight: 400; color: white;">
                 If you have any problems, please contact us: support@moviequotes.ge </p>
-            <p style=" text-align: left; line-height: 150%;  font-size: 16px; font-weight: 400; color:white;">
+            <p
+                style="text-align: left; margin-left: 3rem; line-height: 22px;  font-size: 18px; font-weight: 400; color: white;">
                 MovieQuotes Crew</p>
-
         </div>
     </div>
 
 </body>
-
+z
 
 </html>
