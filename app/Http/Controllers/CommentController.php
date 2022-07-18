69bbc77acc93104dca741d05e5211930595f20d3
chaiqou <lomtadzenikusha@gmail.com>
2022-07-18 16:25:22 +0400

    feat: fetch  single comment and remov unnecessary methods from controllers

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -19,15 +19,6 @@ class CommentController extends Controller
 	}
 
 	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function create()
-	{
-	}
-
-	/**
 	 * Store a newly created resource in storage.
 	 *
 	 * @param \Illuminate\Http\Request $request
@@ -45,19 +36,9 @@ class CommentController extends Controller
 	 *
 	 * @return \Illuminate\Http\Response
 	 */
-	public function show(Comment $comment)
+	public function show(Quote $quote, Comment $comment)
 	{
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param \App\Models\Comment $comment
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function edit(Comment $comment)
-	{
+		return $comment;
 	}
 
 	/**
--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -18,16 +18,6 @@ class LikeController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -45,17 +35,6 @@ class LikeController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Like $like)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Like  $like
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Like $like)
     {
         //
     }
