5852743270700c974accdaf2a5e9c71606e76620
chaiqou <lomtadzenikusha@gmail.com>
2022-07-20 14:42:24 +0400

    feat: create Like notifications

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,18 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\Quote;
+use App\Notifications\NewLikeNotification;
 
 class LikeController extends Controller
 {
 	public function like(Quote $quote)
 	{
-		$quote->like()->create([
+		$quote = $quote->like()->create([
 			'user_id' => auth()->id(),
+		]);
+
+		$user = $quote->user;
+		$user->notify(new NewLikeNotification($quote));
+
+		return response()->json([
+			'message' => 'Quote liked successfully',
+			'quote'   => $quote,
 		]);
 	}
 
 	public function unlike(Quote $quote)
 	{
-		$quote->like()->where('user_id', auth()->id())->first()->delete();
+		$quote = $quote->like()->where('user_id', auth()->id())->first()->delete();
+
+		return response()->json([
+			'message' => 'Quote unliked successfully',
+			'quote'   => $quote,
+		]);
 	}
 }
---
+++ app/Notifications/NewLikeNotification.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Like;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+
+class NewLikeNotification extends Notification
+{
+	use Queueable;
+
+	public $like;
+
+	/**
+	 * Create a new notification instance.
+	 *
+	 * @return void
+	 */
+	public function __construct(Like $like)
+	{
+		$this->like = $like;
+	}
+
+	/**
+	 * Get the notification's delivery channels.
+	 *
+	 * @param mixed $notifiable
+	 *
+	 * @return array
+	 */
+	public function via($notifiable)
+	{
+		return ['database'];
+	}
+
+	/**
+	 * Get the array representation of the notification.
+	 *
+	 * @param mixed $notifiable
+	 *
+	 * @return array
+	 */
+	public function toArray($notifiable)
+	{
+		return [
+			'likeBy'     => $this->like->user->name,
+			'id'         => $this->like->id,
+			'created_at' => $this->like->created_at->diffForHumans(),
+		];
+	}
+}
