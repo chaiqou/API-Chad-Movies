b816183eb13efd94c94c903c379472e5e989ba67
chaiqou <lomtadzenikusha@gmail.com>
2022-07-22 11:11:05 +0400

    feat: add user images logic

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,6 +15,11 @@ class UserResource extends JsonResource
 	 */
 	public function toArray($request)
 	{
-		return parent::toArray($request);
+		return [
+			'id'            => $this->id,
+			'name'          => $this->name,
+			'email'         => $this->email,
+			'profile_image' => new UserImageResource($this->profileImage),
+		];
 	}
 }
