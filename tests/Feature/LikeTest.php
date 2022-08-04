ffdf62fb7f8e23087fe39d41b61b5d55948c2eb6
chaiqou <lomtadzenikusha@gmail.com>
2022-08-04 18:59:06 +0400

    test: like resource

--- tests/Feature/LikeTest.php
+++ tests/Feature/LikeTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Events\LikeEvent;
+use App\Http\Resources\LikeResource;
 use Tests\TestCase;
 use App\Models\Like;
 use App\Models\User;
@@ -59,5 +60,12 @@ class LikeTest extends TestCase
 		$quote = Quote::factory()->create();
 		$like = Like::factory()->create(['quote_id' => $quote->id]);
 		$this->assertTrue($like->quote->is($quote));
+	}
+
+	public function test_like_resource_returns_collection()
+	{
+		$like = Like::factory()->create();
+		$collect = LikeResource::collection(Like::all())->resolve();
+		$this->assertCount(1, $collect);
 	}
 }
