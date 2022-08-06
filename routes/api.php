4cdfe89f6ce1eed0d7f8792c3a8ae774d49c3c93
chaiqou <lomtadzenikusha@gmail.com>
2022-08-06 16:26:51 +0400

    chore: add quote comment post/get method documentations

--- routes/api.php
+++ routes/api.php
@@ -28,10 +28,8 @@ use App\Http\Controllers\Auth\SocialAuthController;
 
 	Route::post('register', [AuthController::class, 'register'])->name('user.register');
 	Route::post('login', [AuthController::class, 'login'])->name('user.login');
-
 	Route::get('authorize/google/redirect', [SocialAuthController::class, 'redirectToProvider'])->name('user.social.register');
 	Route::get('authorize/google/callback', [SocialAuthController::class, 'handleProviderCallback'])->name('user.social.callback');
-
 	Route::get('email-verification', [VerificationController::class, 'index'])->name('verification.verify');
 	Route::post('forgot-password', [ForgotPasswordController::class, 'sendEmail'])->name('user.forgot-password');
 	Route::post('reset-password', [UpdatePasswordController::class, 'updatePassword'])->name('user.reset-password');
@@ -39,23 +37,18 @@ use App\Http\Controllers\Auth\SocialAuthController;
 Route::group(['middleware' => ['auth:api']], function () {
 	Route::apiResource('movies', MovieController::class);
 	Route::apiResource('quotes', QuoteController::class);
+	Route::apiResource('users', UserController::class);
 	Route::apiResource('quotes/{quote}/comment', CommentController::class);
 
-	Route::get('users', [UserController::class, 'index'])->name('user.index');
 	Route::post('logout', [AuthController::class, 'logout'])->name('user.logout');
 	Route::post('checkToken', [AuthController::class, 'checkToken'])->name('user.checkToken');
-
 	Route::get('genres', [GenreController::class, 'index'])->name('genres.index');
-
 	Route::get('movie-slug/{id}', [MovieController::class, 'showBySlug'])->name('movie.getBySlug');
-
 	Route::put('profile', [UserController::class, 'update'])->name('user.profile.update');
+	Route::get('search', [SearchController::class, 'search'])->name('user.search');
 
 	Route::post('like/{quote}', [LikeController::class, 'like'])->name('quote.like');
 	Route::delete('like/{quote}', [LikeController::class, 'unlike'])->name('quote.unlike');
-
-	Route::get('search', [SearchController::class, 'search'])->name('user.search');
-
 	Route::post('notifications', [NotificationController::class, 'index'])->name('notification.index');
 	Route::post('markAsRead', [NotificationController::class, 'read'])->name('notification.read');
 	Route::post('markAllAsRead', [NotificationController::class, 'readAll'])->name('notification.readAll');
